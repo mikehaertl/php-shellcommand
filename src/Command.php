9355d4f7a87bc4ecee5cdee24de389374d2594f3
Michael Härtl <haertl.mike@gmail.com>
2015-04-15 19:04:35 +0200

    Improve proc_open pipe configuration

--- src/Command.php
+++ src/Command.php
@@ -283,7 +283,7 @@ class Command
         } else {
             $descriptors = array(
                 1   => array('pipe','w'),
-                2   => array('pipe','w'),
+                2   => array('pipe','a'),
             );
             $process = proc_open($command, $descriptors, $pipes, $this->procCwd, $this->procEnv, $this->procOptions);
 
