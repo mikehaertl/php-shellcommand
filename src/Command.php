903ee95d3ee8f65ebbe4c6e17705d1d91760521a
Michael Härtl <haertl.mike@gmail.com>
2018-07-07 09:35:36 +0200

    Remove version string

--- src/Command.php
+++ src/Command.php
@@ -7,7 +7,6 @@ namespace mikehaertl\shellcommand;
  * This class represents a shell command.
  *
  * @author Michael Härtl <haertl.mike@gmail.com>
- * @version 1.2.2
  * @license http://www.opensource.org/licenses/MIT
  */
 class Command
